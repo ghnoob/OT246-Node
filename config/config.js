2e44643b56a9e769bff17fa04c2c7216359134b6
Rodrigo Pietnechuk <ptnchk.rodrigo@gmail.com>
2022-08-13 00:08:45 -0300

    fix: use ssl in production

--- config/config.js
+++ config/config.js
@@ -18,5 +18,11 @@ module.exports = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 }
